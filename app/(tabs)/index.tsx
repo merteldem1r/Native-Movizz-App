f796f82d9caf290381b7c2c0b34e100e6cd712fe
Mert Eldemir <mert1greatest@gmail.com>
2025-11-25 02:37:43 +0300

    tabs & general folder file structure & assets

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,9 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-blue-500">
-        Welcome to the Movie APP by Mert Eldemir
-      </Text>
+      <Text className="text-5xl text-dark-200">Welcome!</Text>
     </View>
   );
 }
